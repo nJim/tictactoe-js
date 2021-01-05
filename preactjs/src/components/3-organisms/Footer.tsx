e41fdc190007d2ea6bd7784ba46db2acbfbda713
Jim Vomero <jim.vomero@fourkitchens.com>
2021-01-05 13:32:57 -0500

    preact: Removing assets from template.

--- preactjs/src/components/3-organisms/Footer.tsx
+++ preactjs/src/components/3-organisms/Footer.tsx
@@ -1,10 +1,11 @@
 import { FunctionalComponent, h } from "preact";
 import * as style from "./Footer.css";
+import * as global from "../0-base/global.css";
 
 const Footer: FunctionalComponent = () => (
   <div class={style.footer}>
     Preact project by&nbsp;
-    <a className="funderline" href="http://www.jimvomero.com">Jim Vomero</a>
+    <a className={global.funderline} href="http://www.jimvomero.com">Jim Vomero</a>
   </div>
 );
 
--- preactjs/src/components/4-templates/AboutPage.tsx
+++ preactjs/src/components/4-templates/AboutPage.tsx
@@ -1,14 +1,15 @@
 import { FunctionalComponent, h } from "preact";
 import Border from "../3-organisms/Border";
+import * as style from "../0-base/global.css";
 
 const AboutPage: FunctionalComponent = () => (
   <Border>
     <div>
-      This is a sandbox project for experimenting with React. It&lsquo;s written in TypeScript and includes custom react hooks, context, and state management.
+      This is a sandbox project for experimenting with Preact, a fast 3kB alternative to React with the same ES6 API. It&rsquo;s written in TypeScript and includes custom preact hooks, context, and state management.
       <br /><br />
-      Part of this project is to compare ease of development, performance, and build size  between multiple frameworks. I&lsquo;ll publish the findings in a future blog post.
+      Part of this project is to compare ease of development, performance, and build size  between multiple frameworks. I&rsquo;ll publish the findings in a future blog post.
       <br /><br/>
-      <a class="funderline" href="https://github.com/nJim/tictactoe-react">View this project on GitHub</a>.
+      <a class={style.funderline} href="https://github.com/nJim/tictactoe-react">View this project on GitHub</a>.
     </div>
   </Border>
 );
